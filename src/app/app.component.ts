ec4dc0f39accabe9aa85b70aed688a46b5338bfd
Stefan Jaroni <sjaroni@outlook.de>
2025-02-10 22:37:25 +0100

    add several examples of data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-data-binding';
+
+  // one way data-binding
+  inputValueOne: string = 'banana';
+
+  // two way data-binding
+  inputValueTwo: string = 'in a box';
+  inputValueTwoChange($event: KeyboardEvent) {
+    // console.log($event);
+    const value = ($event.target as HTMLInputElement).value;
+    // console.log(value);
+    this.inputValueTwo = value;
+  }
+
+  // ngmodel
+  inputValueThree: string = 'lemon';
+  inputValueThreeChange(value: string) {
+    // console.log(value);
+    this.inputValueThree = value;
+  }
+
+  // form
+  searchForm = {
+    searchValue: 'kiwi',
+  };
+  onSearchSubmit(): void {
+    console.log(this.searchForm);
+  }
 }
